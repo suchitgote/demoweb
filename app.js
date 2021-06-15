6d97560de3e4c8433d4feda6b879d6856ae29e38
suchit <suchitgote21@gmail.com>
2021-06-16 01:18:49 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port =  5000
+
+app.set('view engine', 'html');
+
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', (req, res) => {
+   res.render("index.html")
+})
+
+app.get('/about', (req, res) => {
+    res.render("about.html")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
